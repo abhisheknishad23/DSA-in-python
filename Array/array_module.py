caf5da7301468d23d8b98606bd5baef5de2fbc6b
abhisheknishad23 <iabhisheku4@gmail.com>
2026-04-03 19:36:07 +0530

    arr

--- Array/array_module.py
+++ Array/array_module.py
@@ -1,8 +1,21 @@
 #python array module
 
+#from array import *
+#import array as arr
+
 import array
 
 val=array.array('i',[1,2,3,4,5,6])
 
+for i in range(0,6):
+    print(val[i], end=' ')
+
+print('\n')
+
+for x in val:
+    print(x, end=' ,')
+
+
+val.reverse()
 for i in range(0,6):
     print(val[i], end=' ')
