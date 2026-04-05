22c9fb10b277f7e4e12b4868fcb0a7d580ff390d
abhisheknishad23 <iabhisheku4@gmail.com>
2026-04-05 20:56:09 +0530

    sll

---
+++ Array/D_arr.py
@@ -0,0 +1,19 @@
+from numpy import *
+
+zero=array(10)
+print(zero)
+
+print('\n')
+#one diamension array
+one=array([1,2,3,4,5])
+print(one)
+
+print('\n')
+#two diamension array
+two = array([[1,2,3],[4,5,6],[7,8,9]])
+print(two)
+
+print('\n')
+#three diamension array
+three = array([[[1,2],[3,4],[5,6],[7,8]]])
+print(three)
