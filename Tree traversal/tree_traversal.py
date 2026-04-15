67fa43c8c042113c9dbba73fbaa863f14cbc0da0
abhisheknishad23 <iabhisheku4@gmail.com>
2026-04-15 20:16:21 +0530

    tt

---
+++ Tree traversal/tree_traversal.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,value):
+        self.left=None
+        self.right=None
+        self.data=value
+
+def preOrder(root):
+    if(root!=None):
+        print(root.data, end="")
+        preOrder(root.left)
+        preOrder(root.right)
+
+def InOrder(root):
+    if(root!=None):
+        InOrder(root.left)
+        print(root.data, end="")
+        InOrder(root.right)
+
+def PostOrder(root):
+    if(root!=None):
+        PostOrder(root.left)
+        PostOrder(root.right)
+        print(root.data, end="")
+
+
+root =Node(1)
+root.left=Node(3)
+root.right=Node(5)
+root.left.left=Node(2)
+root.left.right=Node(4)
+root.right.right=Node(8)
+
+InOrder(root)
