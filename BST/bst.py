ff2e35b08ea2c2517cfb4c5a36dd9d00085bfd7e
abhisheknishad23 <iabhisheku4@gmail.com>
2026-04-18 23:36:12 +0530

    bst

---
+++ BST/bst.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self,value):
+        self.left=None
+        self.right=None
+        self.data=value
+
+    def insert(root,value):
+        if(root!=None):
+            return Node(value)
+        if(root.data==value):
+            return root
