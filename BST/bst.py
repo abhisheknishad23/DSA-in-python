d17bf988b34f544aa5066e1ea0f5ff2c7c440378
abhisheknishad23 <iabhisheku4@gmail.com>
2026-04-22 23:34:22 +0530

    b

--- BST/bst.py
+++ BST/bst.py
@@ -4,22 +4,36 @@ class Node:
         self.right=None
         self.data=value
 
-    def insert(root,value):
-        if(root==None):
-            return Node(value)
-        if(root.data==value):
-            return root
-        if(root.data>value):
-            root.left=insert(root.left,value)
-        else:
-            root.right=insert(root.right,value)
+def insert(root,value):
+    if(root==None):
+        return Node(value)
+    if(root.data==value):
         return root
+    if(root.data>value):
+        root.left=insert(root.left,value)
+    else:
+        root.right=insert(root.right,value)
+    return root
+
+def search(root,value):
+    if(root==None):
+        print('Element not found',end='\n')
+        return
+    if(root.data==value):
+        print('element found',end='\n')
+        return
+    if(root.data>value):
+        search(root.left,value)
+    else:
+        search(root.left,value)
+
     
-    def InOrder(root):
-        if(root!=None):
-            InOrder(root.left)
-            print(root.data,end=" ")
-            InOrder(root.right)
+    
+def InOrder(root):
+    if(root!=None):
+        InOrder(root.left)
+        print(root.data,end=" ")
+        InOrder(root.right)
 
 root = Node(20)
 root.left=Node(15)
